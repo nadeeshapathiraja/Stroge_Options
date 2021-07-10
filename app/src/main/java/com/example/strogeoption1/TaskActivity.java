922e5a85e516110c6d21f59f780780a5ebbf5c78
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-07-10 23:20:35 +0530

    SQLite stroge option

--- app/src/main/java/com/example/strogeoption1/TaskActivity.java
+++ app/src/main/java/com/example/strogeoption1/TaskActivity.java
@@ -3,6 +3,7 @@ package com.example.strogeoption1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class TaskActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class TaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+    }
+    public void createTask(View v){
+
     }
 }
