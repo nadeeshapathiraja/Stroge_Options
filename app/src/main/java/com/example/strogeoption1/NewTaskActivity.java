c4ff51bdc19889241454f91d99593c3a25c9986b
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-07-12 19:54:18 +0530

    Add functions

--- app/src/main/java/com/example/strogeoption1/NewTaskActivity.java
+++ app/src/main/java/com/example/strogeoption1/NewTaskActivity.java
@@ -3,12 +3,32 @@ package com.example.strogeoption1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class NewTaskActivity extends AppCompatActivity {
+
+    EditText etname;
+    DatePicker taskDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+        etname = findViewById(R.id.taskname);
+        taskDate = findViewById(R.id.datePicker);
+    }
+
+    public void save(View v){
+        String name = etname.getText().toString();
+        String date = taskDate.toString();
+
+
+
+
+    }
+    public void clear(View v){
+        etname.setText("");
     }
 }
--- app/src/main/java/com/example/strogeoption1/TaskHelper.java
+++ app/src/main/java/com/example/strogeoption1/TaskHelper.java
@@ -1,4 +1,29 @@
 package com.example.strogeoption1;
 
-public class TaskHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class TaskHelper extends SQLiteOpenHelper {
+
+
+     public TaskHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, "dbtasks", null, 1);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+         //Genarate Query
+        String sql1 = "CREATE TABLE tasks (_id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT, date DATE, complete BOOLEAN)";
+        //pass query to db
+        db.execSQL(sql1);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
