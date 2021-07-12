fd1be6b94fb09e81b3bad7ae23bf2a8f99d0666e
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-07-12 22:00:49 +0530

    Add SQLite db full

--- app/src/main/java/com/example/strogeoption1/NewTaskActivity.java
+++ app/src/main/java/com/example/strogeoption1/NewTaskActivity.java
@@ -2,10 +2,12 @@ package com.example.strogeoption1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class NewTaskActivity extends AppCompatActivity {
 
@@ -24,7 +26,21 @@ public class NewTaskActivity extends AppCompatActivity {
         String name = etname.getText().toString();
         String date = taskDate.toString();
 
+        //Step 1. Create openHelper object with context passed
+        TaskHelper dbhelper = new TaskHelper(this,"dbtasks",null,1);
 
+        //Step 2. Get Writeble database object
+        SQLiteDatabase db = dbhelper.getWritableDatabase();
+
+        //Step 3. Create Insert SQL query
+        String query = "INSERT INTO tasks (name,date,complete) VALUES ('"+name+"','"+date+"','0')";
+
+        //Step 3. Execute Query
+        db.execSQL(query);
+
+        //Step 4. Give a Message
+        Toast.makeText(this, "Save Successfully", Toast.LENGTH_SHORT).show();
+        clear(v);
 
 
     }
