247bee7ac7261407cd22fcd6078d3aad2d7f64a4
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-07-14 03:56:39 +0530

    Project test

--- app/src/main/java/com/example/strogeoption1/TaskActivity.java
+++ app/src/main/java/com/example/strogeoption1/TaskActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -55,9 +56,18 @@ public class TaskActivity extends AppCompatActivity {
         //Step 8- Get List view object
         ListView lv = findViewById(R.id.task_list);
 
-        //Step 9- Create adapter and set values to the list
-        int layout = android.R.layout.simple_list_item_1;
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,layout,list);
+//        //Step 9- Create adapter and set values to the list(Default view)
+//        int layout = android.R.layout.simple_list_item_1;
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,layout,list);
+
+        //Step 9-(User layout)
+        int layout = R.layout.one_task;
+        String []columns = {"name","date","id"};
+
+        int[] lableSet = {R.id.taskname,R.id.task_date,R.id.task_id};
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,layout,cursor,columns,lableSet);
+
 
         //Step 10- Set adapter to the list view
         lv.setAdapter(adapter);
